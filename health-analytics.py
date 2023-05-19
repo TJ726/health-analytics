a0e4536fa6cda7d236b56d17eaf9fde4a92f9817
Tejasvi Medi <teejay@Tejasvis-MacBook-Pro.local>
2023-05-19 10:12:08 -0700

    modified patient data

--- health-analytics.py
+++ health-analytics.py
@@ -3,14 +3,16 @@
 import pyspark
 import pandas as pd
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, col, concat_ws
+from pyspark.sql.functions import avg, col, concat_ws, when
 from datetime import datetime
 import boto3
 
+# COMMAND ----------
 
 ## Creating a spark session
 spark = SparkSession.builder.config("spark.jars.packages", "com.crealytics:spark-excel_2.12:0.13.7").getOrCreate()
 
+# COMMAND ----------
 
 ## Loading aws access keys in a dataframe
 aws_keys_df = spark.read.format('csv').option('header','true').option('inferSchema','true').load('/FileStore/tables/TejasviMedi_accessKeys.csv')
@@ -18,7 +20,7 @@ aws_keys_df = spark.read.format('csv').option('header','true').option('inferSche
 ACCESS_KEY = aws_keys_df.select('Access key ID').take(1)[0]['Access key ID']
 SECRET_KEY = aws_keys_df.select('Secret access key').take(1)[0]['Secret access key']
 
-
+# COMMAND ----------
 
 ## Setting up configuration to make connection with S3
 bucket_name='aws-patient-health-analytics'
@@ -27,29 +29,23 @@ file_path = "s3a://{}/input/exercise-data.xlsx".format(bucket_name)
 spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", ACCESS_KEY)
 spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", SECRET_KEY)
 
-
+# COMMAND ----------
 
 # import urllib
-
 # ENCODED_SECRET_KEY = urllib.parse.quote(string = SECRET_KEY,safe="")
-
 # AWS_S3_BUCKET = 'aws-patient-health-analytics'
 # MOUNT_NAME = '/mnt/mount_s3'
-
 # SOURCE_URL = f's3a://{ACCESS_KEY}:{ENCODED_SECRET_KEY}@{AWS_S3_BUCKET}'
 
-
-
 # dbutils.fs.mount(SOURCE_URL,MOUNT_NAME)
-
 # %fs ls '/mnt/mount_s3'
 
-
+# COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## Extract Data
 
-
+# COMMAND ----------
 
 ## Since data is in an excel sheet, used crealytics library to load the file
 ## Looped through the file names and created all the required Dataframes
@@ -60,72 +56,90 @@ for sheet_name in sheet_names:
     .option("dataAddress", sheet_name+"!").option("header", "true")\
     .option("inferSchema", "true").load(file_path)
 
-
+# COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## Clean up the tables
 # MAGIC ##### Removing Duplicate rows
 # MAGIC ##### stripping columns we do not need
 
-
+# COMMAND ----------
 
 medications_e1.count()
+
+# COMMAND ----------
 
 medications_e1 = medications_e1.dropDuplicates()
 medications = medications_e1.select(['encounterid','patientid','medication_simple_generic_name','minimum_dose','dose_unit'])
 
+# COMMAND ----------
+
 medications_e1.count()
+
+# COMMAND ----------
 
 medications = medications.na.drop(subset = ['minimum_dose'])
 medications.count()
 
+# COMMAND ----------
+
 encounter = encounter_e1.select(['patientid','encounterid','admit_diagnosis'])
 patient = patient_e1.select(['patientid','Sex','Age','primary_care_provider'])
 
+# COMMAND ----------
 
+patient = patient.withColumn("Age", when(col("Age") > 100, None).otherwise(col("Age")))
+
+# COMMAND ----------
+
+average_age = patient.select(avg("Age")).first()[0]
+patient = patient.fillna({'Age':average_age})
+
+# COMMAND ----------
+
+# patient = patient.withColumn("Age", patient["Age"].cast("int"))
+
+# COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## Join the Medications and Encounter tables
 
-
+# COMMAND ----------
 
 med_enc = medications.join(encounter, on=['encounterid','patientid'],how='inner')
 
 med_enc.printSchema()
-med_enc.show(5)
 
-
+# COMMAND ----------
 
 med_enc.count()
 
-
+# COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## Perform the grouping operation
 
-
+# COMMAND ----------
 
 med_enc_grouped = med_enc.groupBy('patientid','medication_simple_generic_name','dose_unit','admit_diagnosis').agg(avg("minimum_dose").alias("avg_minimum_dose"))
 
-
-
-med_enc_grouped.show(10)
-
-
+# COMMAND ----------
 
 patient_meds = patient.join(med_enc_grouped, on='patientid', how="inner")
-
 patient_meds.printSchema()
+patient_meds.count()
 
-
+# COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## Converting all the columns to String
 
+# COMMAND ----------
 
-
+## Converting patientid and primary_care_provider to Decimal so it doesn't change to scientific numbers.
 patient_meds = patient_meds.withColumn("patientid", col('patientid').cast("Decimal(14,0)")).withColumn("primary_care_provider", col('primary_care_provider').cast("Decimal(14,0)"))
 
+## casting all columns to string
 for col_name, col_type in patient_meds.dtypes:
     if col_type != "string":
         patient_meds = patient_meds.withColumn(col_name, col(col_name).cast("string"))
@@ -134,19 +148,19 @@ patient_meds.printSchema()
 patient_meds.show(5)
 
 
-
+# COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Changing the dataframe to a single string column
+# MAGIC ## Storing Dataframe into a text file on S3
 
-
+# COMMAND ----------
 
 date_str = datetime.now().strftime("%Y%m%d")
 output_file = f"tmp/target_1"
 # local_output = "/dbfs/FileStore/tables/result"
 final_output = f"output/target_1_{date_str}.txt"
 
-
+# COMMAND ----------
 
 # file_key = 'path/to/your/target_1_YYYYMMDD.txt'
 field_delimiter = '|'
@@ -157,9 +171,7 @@ patient_meds.coalesce(1).write.mode("overwrite").option("header", True)\
   .option("quote", text_qualifier)\
   .option("encoding", "UTF-8").option("wholetext","true").csv(f"s3a://{bucket_name}/{output_file}")
 
-#   .csv(f"s3a://{bucket_name}/{output_file}")
-
-
+# COMMAND ----------
 
 s3 = boto3.client('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
 
@@ -167,26 +179,26 @@ response = s3.list_objects_v2(Bucket=bucket_name,Prefix='tmp/')
 
 objects = [object['Key'] for object in response['Contents'] if object['Key'].endswith('.csv')]
 
-
+# COMMAND ----------
 
 objects[0]
 
-
+# COMMAND ----------
 
 result = s3.copy_object(Bucket=bucket_name,Key=final_output,CopySource={'Bucket':bucket_name,'Key':objects[0]})
 
-
+# COMMAND ----------
 
 objects = [s3.delete_object(Bucket=bucket_name,Key=object['Key']) for object in response['Contents']]
 
-
+# COMMAND ----------
 
 # delimiter = "|"
 # text_qualifier = '"'
 # header = text_qualifier + delimiter.join(patient_meds.columns) + text_qualifier
 # header
 
-
+# COMMAND ----------
 
 # df_string = patient_meds.select(concat_ws(delimiter, *[col(c).cast("string") for c in patient_meds.columns]).alias(header))
 # df_string.coalesce(1).write.text(output_file)
@@ -196,6 +208,6 @@ objects = [s3.delete_object(Bucket=bucket_name,Key=object['Key']) for object in 
 # dbutils.fs.cp(file_path, final_output)
 # dbutils.fs.rm(output_file, recurse=True)
 
-
+# COMMAND ----------
 
 spark.stop()
